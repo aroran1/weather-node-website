45dc2e05e1ee4d09f8b8c36a02a30dd24630650e
aroran1 <nidhigulati2001@gmail.com>
2021-01-31 01:33:36 +0000

    web-server: added ui rendering of the weather fetch

--- public/js/app.js
+++ public/js/app.js
@@ -27,18 +27,45 @@ const fetchWeather = (url) => fetch(url)
   if(data.error) {
     console.log(data.error);
   } else {
-    console.log(data.location);
-    console.log(data.msg);
+    // console.log(data.location);
+    console.log(data);
+    return data;
   }
 })
 
 const weatherForm = document.querySelector('form');
 const search = document.querySelector('input');
+const msgOne = document.querySelector('#msg-1');
+const msgTwo = document.querySelector('#msg-2');
+
+
 weatherForm.addEventListener('submit', (e) => {
   e.preventDefault();
+  msgOne.textContent = 'Loading...';
+  msgTwo.textContent = '';
   const location = search.value;
   console.log(location);
 
   const url = `/weather?address=${location}`
-  fetchWeather(url);
+
+  // returning data from fetchWeather isn't working!
+  // const fetchedData = fetchWeather(url);
+  // console.log('fetchedData', fetchedData);
+
+  fetch(url)
+  .then(response => {
+    return response.json();
+  })
+  .then(data => {
+    if(data.error) {
+      console.log(data.error);
+      msgOne.textContent = 'Ooops.., There is an Error';
+      msgTwo.textContent = data.error;
+    } else {
+      // console.log(data.location);
+      console.log(data);
+      msgOne.textContent = data.location;
+      msgTwo.textContent = data.msg;
+    }
+  });
 });
